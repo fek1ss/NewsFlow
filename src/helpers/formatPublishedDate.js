2539ce0d7ed89f553b7d94bd0e6642ef8e20a7b1
fek1ss <140281721+avantuurist@users.noreply.github.com>
2025-02-19 08:20:03 +0600

    format date is changed

--- src/helpers/formatPublishedDate.js
+++ src/helpers/formatPublishedDate.js
@@ -1,5 +1,4 @@
 export const formatPublishedDate = (published) => {
-  if(!published) return "uknown date";
+  if (!published) return 'uknown date';
   return published.slice(0, 16);
-}
->>>>>>> develop
+};
